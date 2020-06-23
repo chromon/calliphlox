dbdd15db1a017225e59a3ee7bb14cd5fd1304db0
chromon <allotory@msn.com>
2020-06-23 20:09:23 +0800

    feat: update schema test

--- test/schema_test.go
+++ test/schema_test.go
@@ -16,10 +16,10 @@ var TestDial, _ = dialect.GetDialect("mysql")
 
 func TestParse(t *testing.T) {
 	s := schema.Parse(&User{}, TestDial)
-	if s.Name != "User" || len(s.Fields) != 2 {
+	if s.Name != "User" || len(s.Fields) != 3 {
 		t.Fatal("failed to parse User struct")
 	}
-	if s.GetField("Name").Tag != "PRIMARY KEY" {
+	if s.GetField("Id").Tag != "PRIMARY KEY" {
 		t.Fatal("failed to parse primary key")
 	}
 }
